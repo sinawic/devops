02a5d774c0670dc815893425817cf82948d5c760
sinawic <sina_041_sm@yahoo.com>
2022-10-27 10:24:19 +0330

    postgres backup restore

--- postgre.sh
+++ postgre.sh
@@ -51,3 +51,17 @@ create user myuser with encrypted password 'mypass';
 
 # Grant access to user
 grant all privileges on database mydb to myuser;
+
+#######################################################
+
+# Backup Large PostgreSQL Databases
+pg_dump likeit | gzip > likeit.gz
+
+# If the database is extremely large, you can dump in parallel by dumping number_of_jobs tables simultaneously using the -j flag
+pg_dump -F d -j 5 -f likeit
+
+# dump specific database
+PGPASSWORD="password" pg_dump -h HOST -U root -p PORT likeit > likeit.sql
+
+# Restoring a PostgreSQL Database
+psql likeit < likeit.sql
